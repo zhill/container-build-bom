ea4c46fba0e5a6b1ca4aa6e746dc9a01a42296f2
Zach Hill <zach@anchore.com>
2020-03-06 16:12:57 -0800

    Use cli framework and use pip to find packages

--- cmd/bom.go
+++ cmd/bom.go
@@ -1,57 +1,105 @@
 package main
 
 import (
+	set "github.com/deckarep/golang-set"
 	"encoding/json"
+	"fmt"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 )
 
-func FindPip() ([]string, error) {
-	// Run pip freeze
-	return []string{"pip1"}, nil
+type Package struct {
+	Name     string
+	Version  string
+	Location string
+	Type     string
+	Licenses []string
+	Size     int
+	Language string
 }
 
-func FindNpm() ([]string, error) {
+func FindPip() ([]Package, error) {
+	// This is intended for post-build, so assumes you have pip installed in the container itself.
+	pips := make([]interface{}, 0)
+
+	for _, name := range []string{"pip", "pip2", "pip3"} {
+		cmd := exec.Command(name, "freeze", "--all")
+		log.Println("Executing ", cmd.String())
+
+		output, err := cmd.Output()
+		if err != nil {
+			return nil, err
+		}
+
+		for _, line := range strings.Split(string(output), "\n") {
+			pair := strings.Split(line, "==")
+			if len(pair) == 2 {
+				pips = append(pips, Package{
+					Name:     pair[0],
+					Version:  pair[1],
+					Location: "",
+					Language: "python",
+					Type:     "pip",
+					Size:     -1,
+					Licenses: []string{},
+				})
+			}
+		}
+	}
+
+	pipsSet := set.NewSetFromSlice(pips)
+	s := pipsSet.ToSlice()
+	return []Package(s), nil
+}
+
+
+func FindNpm() ([]Package, error) {
 	// Find all package-lock.json files
-	return []string{"npm1"}, nil
+
+	return []Package{}, nil
 }
 
 func analyze(c *cli.Context) error {
 	var err error
-	result := map[string][]string {
-		"pip": nil,
-		"npm": nil,
-	}
+	var result []Package
 
-	result["pip"], err = FindPip()
+	pips, err := FindPip()
+	if err != nil {
+		return err
+	}
+	result = append(result, pips...)
+
+	npms, err := FindNpm()
 	if err != nil {
 		return err
 	}
 
-	result["npm"], err = FindNpm()
-	if err != nil {
-		return err
-	}
+	result = append(result, npms...)
 
 	var d []byte
 	d, err = json.Marshal(result)
-	log.Println("Results: ", string(d))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(d))
 	return nil
 }
 
-
 func main() {
 	app := cli.App{
-		Name: "container-bom",
+		Name:  "container-bom",
 		Usage: "Constructs a single BOM json result for artfacts found in the local filesystem",
-		Commands: []*cli.Command {
+		Commands: []*cli.Command{
 			{
-				Name: "analyze",
+				Name:    "analyze",
 				Aliases: []string{"a"},
-				Usage: "Detect all supported artifact types and output result in json form to stdout",
-				Flags: []cli.Flag{},
-				Action: analyze,
+				Usage:   "Detect all supported artifact types and output result in json form to stdout",
+				Flags:   []cli.Flag{},
+				Action:  analyze,
 			},
 		},
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/zhill/container-build-bom
 
 go 1.13
 
-require github.com/urfave/cli/v2 v2.1.1
+require (
+	github.com/deckarep/golang-set v1.7.1
+	github.com/urfave/cli/v2 v2.1.1
+)
